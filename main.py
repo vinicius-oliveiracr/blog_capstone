c3831fc6ff22b33c3264fcc56952fb8be22d6da6
vinicius-oliveiracr <vinicius.cr.o.3@gmail.com>
2024-06-26 14:37:38 -0300

    initial commit

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
+from dotenv import load_dotenv
 
 
 def admin_only(f):
@@ -21,8 +23,11 @@ def admin_only(f):
     return decorated_function
 
 
+load_dotenv()
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -42,7 +47,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -262,4 +267,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
